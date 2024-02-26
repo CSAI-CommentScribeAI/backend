282c50c40d1a19be87e5e7507ebb7b78a9f356fd
youdaeun <dbekdms586@gmail.com>
2024-02-26 13:28:56 +0900

    fix : 매장/등록/삭제/수정
    user 파일 delete

--- src/main/java/com/example/backend/deliveryapp/user/owner/repository/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.deliveryapp.user.owner.repository;
-
-import com.example.backend.deliveryapp.user.owner.entity.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-}
