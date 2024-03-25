16b875d6152ceb94d1ce9d6d8e95f57c4b519fc8
josephuk77 <josephuk77@gmail.com>
2024-03-25 12:03:14 +0900

    feat : redis와 refresh토큰을 활용해 로그인 기능 강화
    redis를 사용해서 refresh토큰을 저장하고 token이 만료될 때 refresh토큰으로 재발급 받아 로그인이 연장될 수 있게 추가

--- src/main/java/com/example/backend/controller/auth/AuthController.java
+++ src/main/java/com/example/backend/controller/auth/AuthController.java
@@ -5,6 +5,7 @@ import com.example.backend.UserAccount.dto.UserAccountRequestDto;
 import com.example.backend.UserAccount.dto.UserAccountResponseDto;
 import com.example.backend.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+
     @PostMapping("/signup")
     public ResponseEntity<UserAccountResponseDto> signup(@RequestBody UserAccountRequestDto requestDto) {
         return ResponseEntity.ok(authService.signup(requestDto));
@@ -26,5 +28,21 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody UserAccountRequestDto requestDto) {
         return ResponseEntity.ok(authService.login(requestDto));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenDto> refresh(@RequestBody TokenDto tokenDto) {
+        TokenDto newTokenDto = authService.refresh(tokenDto.getRefreshToken());
+        if (newTokenDto != null) {
+            return ResponseEntity.ok(newTokenDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestBody TokenDto tokenDto) {
+        authService.logout(tokenDto.getRefreshToken());
+        return ResponseEntity.ok().build();  // 성공적인 로그아웃 처리 응답
     }
 }
